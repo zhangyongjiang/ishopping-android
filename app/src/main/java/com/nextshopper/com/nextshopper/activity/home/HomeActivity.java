c9ebd6266329ba40d1420cd29ecfdbb83b27dd64
Kevin Zhang <zhangyongjiang@gmail.com>
2015-07-04 20:26:49 -0700

    Product Grid

--- app/src/main/java/com/nextshopper/com/nextshopper/activity/home/HomeActivity.java
+++ app/src/main/java/com/nextshopper/com/nextshopper/activity/home/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.nextshopper.com.nextshopper.activity.home;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 import com.nextshopper.activity.BaseActivity;
+import com.nextshopper.activity.R;
 
 /**
  * Created by Zhang_Kevin on 7/4/15.
@@ -11,5 +13,8 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_grid);
+
+        GridView gridView = (GridView)findViewById(R.id.product_grid_view);
     }
 }
