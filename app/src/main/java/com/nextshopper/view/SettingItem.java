1ec1c8d84318745bac6df6947dc9fb6c9afb3c34
Siyi <siyi.liu001@gmail.com>
2015-08-04 03:28:17 -0700

    setting page

---
+++ app/src/main/java/com/nextshopper/view/SettingItem.java
@@ -0,0 +1,24 @@
+package com.nextshopper.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.nextshopper.activity.R;
+
+
+public class SettingItem extends FrameLayout {
+
+   private TextView item;
+
+    public SettingItem(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.setting_item, this);
+        item = (TextView) findViewById(R.id.setting_item);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.SettingItem);
+        item.setText(ta.getString(R.styleable.SettingItem_item));
+    }
+}
