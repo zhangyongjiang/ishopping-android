aadc705288a1e2b17a0d5d597219ef7435c549c0
Siyi <siyi.liu001@gmail.com>
2015-08-16 01:36:39 -0700

    product detail page

--- app/src/main/java/com/nextshopper/activity/ProductDetailsActivity.java
+++ app/src/main/java/com/nextshopper/activity/ProductDetailsActivity.java
@@ -17,6 +17,7 @@ public class ProductDetailsActivity extends SwipeRefreshActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
         refresh();
     }
 
