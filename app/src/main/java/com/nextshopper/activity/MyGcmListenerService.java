4dc84b02f5047f87f8acd4f67727b3f40ffe14ec
Siyi <siyi.liu001@gmail.com>
2015-10-05 00:03:43 -0700

    fix

--- app/src/main/java/com/nextshopper/activity/MyGcmListenerService.java
+++ app/src/main/java/com/nextshopper/activity/MyGcmListenerService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Google Inc. All Rights Reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +27,7 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GcmListenerService;
+import com.nextshopper.common.Constant;
 
 public class MyGcmListenerService extends GcmListenerService {
 
@@ -73,15 +74,18 @@ public class MyGcmListenerService extends GcmListenerService {
      * @param message GCM message received.
      */
     private void sendNotification(String message) {
+        // SharedPreferences sharedPreferences = getSharedPreferences(Constant.USER, MODE_PRIVATE);
+        //String cookie =  sharedPreferences.getString(Constant.COOKIE, "")
         Intent intent = new Intent(this, MainActivity.class);
+
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.notification_template_icon_bg)
-                .setContentTitle("GCM Message")
+                .setContentTitle(Constant.NEXTSHOPPER)
                 .setContentText(message)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
